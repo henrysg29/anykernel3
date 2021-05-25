d700437ab3755969d63713ca6d82da77986c5655
henrysg <18201042@ittelkom.pwt.ac.id>
2021-05-25 11:04:25 +0000

    anykernel.sh : change to own brand

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Trashed by henztz
 do.devicecheck=0
 do.modules=0
 do.systemless=1
